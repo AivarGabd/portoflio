67bb9825a1a0d53665f41b58cb4718e2f5a6de78
aivargab <aivargab@mail.ru>
2024-07-16 23:42:14 +0500

    1

--- app/theme-button/page.tsx
+++ app/theme-button/page.tsx
@@ -20,75 +20,71 @@ const Page = () => {
   const [theme, setTheme] = useState<"dark" | "light">("light");
 
   return (
-    <div className="mt-[20vh]">
-      <div
-        className={`full lg:rounded-xl border border-[#191918] px-4 py-6 relative flex min-h-[250px] ${
-          theme == "light" ? "bg-[#DFDFD6] light" : "bg-black dark"
-        }`}
-      >
-        <div className="m-auto">
-          <Button
-            size="lg"
-            variant="flat"
-            radius="sm"
-            isIconOnly
-            onClick={() => {
-              if (theme == "dark") {
-                setTheme("light");
-              } else {
-                setTheme("dark");
-              }
-            }}
-          >
-            {theme == "dark" ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="lucide lucide-moon scale-125"
-              >
+    <ExampleWrapper
+      styles={theme == "light" ? "bg-[#DFDFD6] light" : "bg-black dark"}
+    >
+      <div className="m-auto">
+        <Button
+          size="lg"
+          variant="flat"
+          radius="sm"
+          isIconOnly
+          onClick={() => {
+            if (theme == "dark") {
+              setTheme("light");
+            } else {
+              setTheme("dark");
+            }
+          }}
+        >
+          {theme == "dark" ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-moon scale-125"
+            >
+              <motion.path
+                initial={{ scale: 0.5, opacity: 0.2, rotate: 100 }}
+                animate={{ scale: 1, opacity: 1, rotate: 0 }}
+                transition={{ duration: 0.5 }}
+                d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-sun-medium scale-125"
+            >
+              <circle cx="12" cy="12" r="4" />
+              {sunPaths.map((path, index) => (
                 <motion.path
-                  initial={{ scale: 0.5, opacity: 0.2, rotate: 100 }}
-                  animate={{ scale: 1, opacity: 1, rotate: 0 }}
-                  transition={{ duration: 0.5 }}
-                  d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"
+                  key={index}
+                  initial={{ scale: 0.5, opacity: 0.2 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{ duration: 0.2, delay: (index + 1) * 0.1 }}
+                  d={path}
                 />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="lucide lucide-sun-medium scale-125"
-              >
-                <circle cx="12" cy="12" r="4" />
-                {sunPaths.map((path, index) => (
-                  <motion.path
-                    key={index}
-                    initial={{ scale: 0.5, opacity: 0.2 }}
-                    animate={{ scale: 1, opacity: 1 }}
-                    transition={{ duration: 0.2, delay: (index + 1) * 0.1 }}
-                    d={path}
-                  />
-                ))}
-              </svg>
-            )}
-          </Button>
-        </div>
+              ))}
+            </svg>
+          )}
+        </Button>
       </div>
-    </div>
+    </ExampleWrapper>
   );
 };
 
--- components/ExampleWrapper.tsx
+++ components/ExampleWrapper.tsx
@@ -1,16 +1,23 @@
-'use client'
+"use client";
+
+import { cn } from "@/lib/utils";
 
 const ExampleWrapper = ({
   children,
+  styles,
 }: Readonly<{
   children: React.ReactNode;
+  styles?: string;
 }>) => (
   <div className="mt-[20vh]">
-    <div className="bg-black w-full lg:rounded-xl border border-[#191918] px-4 py-6 relative flex min-h-[250px] ">
+    <div
+      className={cn(
+        `bg-black w-full lg:rounded-xl border border-[#191918] px-4 py-6 relative flex min-h-[250px] ${styles}`
+      )}
+    >
       {children}
     </div>
   </div>
 );
 
-
-export default ExampleWrapper
+export default ExampleWrapper;
